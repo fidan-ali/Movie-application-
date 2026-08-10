07d7a2b2d03e6f66e5840c809e7c334d3d2c5ff2
Fidan <fdnstudi732@gmail.com>
2026-08-10 15:39:25 +0400

    refactor: change return type of existsByEmail method to boolean

--- src/main/java/com/example/movies/dao/repository/UserRepository.java
+++ src/main/java/com/example/movies/dao/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
 
-    Boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
 
     boolean existsByEmailAndIdNot(String email, Long id);
 }
